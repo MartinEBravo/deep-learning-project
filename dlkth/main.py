f1562c540888b3282506c1bcb28ed9fdf1fa0fd4
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2025-05-18 21:46:36 +0200

    fixed main pipeline

--- dlkth/main.py
+++ dlkth/main.py
@@ -1,142 +1,77 @@
 import time
-
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
 
 from dlkth.data_loader import (
     get_rnn_train_loader,
     get_transformer_train_loader,
     preprocess_and_save_data,
     load_preprocess,
+    load_data
 )
 from dlkth.config import Config, ConfigTransformers, config_rnn, config_transformer
 from dlkth.models.rnn import RNN
-from dlkth.models.utils import save_model
 from dlkth.models.transformer import Transformer
-from dlkth.tokenizer import get_tokenizer
+from dlkth.models.bigram import Bigram
+from dlkth.models.utils import save_model
+from dlkth.tokenizer import CharTokenizer
 
 
-def get_model(vocab_size: int, output_size: int, model_name: str = "rnn"):
-    if model_name.lower() == "rnn":
-        config = config_rnn
-        return RNN(
-            vocab_size=vocab_size,
-            output_size=output_size,
-            embedding_dim=config.embedding_dim,
-            hidden_dim=config.hidden_dim,
-            n_layers=config.n_layers,
-            dropout=config.dropout,
-        )
+def train_workflow(model_name: str, dataset: str):
+    # Load text
+    text = load_data(f"data/{dataset}.txt")
 
-    elif model_name.lower() == "transformer":
-        config = config_transformer
-        return Transformer(
-            vocab_size=vocab_size,
-            n_embd=config.embedding_dim,
-            n_head=config.n_head,
-            n_layer=config.n_layers,
-            block_size=config.block_size,
-            dropout=config.dropout,
-        )
-    raise NotImplementedError
+    # Load Tokenizer
+    tokenizer = CharTokenizer(text)
+    encode = tokenizer.encode
+    decode = tokenizer.decode
+    vocab_size = tokenizer.vocab_size
 
+    # Train and test splits
+    data = torch.tensor(encode(text), dtype=torch.long)
+    n = int(0.9 * len(data))
+    train_data = data[:n]
+    val_data = data[n:]
 
-def get_train_loader(
-    tokens: list[int],
-    config: Config,
-    pad_token_id: int,
-    model_name: str = "rnn",
-) -> DataLoader:
-    if model_name.lower() == "rnn":
-        return get_rnn_train_loader(
-            tokens=tokens,
-            sequence_length=config.text_sequence_length,
-            batch_size=config.text_batch_size,
-        )
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    elif model_name.lower() == "transformer":
-        assert isinstance(config, ConfigTransformers)
-
-        return get_transformer_train_loader(
-            tokens=tokens,
-            sequence_length=config.text_sequence_length,
-            batch_size=config.text_batch_size,
-            block_size=config.block_size,
-            pad_token_id=pad_token_id,
-        )
-
-    raise NotImplementedError
-
-def run_workflow(
-    model_name: str = "rnn",  # "rnn" or "transformer"
-):
-    if model_name.lower() == "rnn":
-        config = config_rnn
+    # Model instantiation
+    if model_name == 'bigram':
+        model = Bigram(vocab_size).to(device)
+    elif model_name == 'rnn':
+        raise NotImplementedError("RNN training not implemented yet")
+    elif model_name == 'transformers':
+        raise NotImplementedError("Transformer training not implemented yet")
     else:
-        config = config_transformer
-    # Tokenization
-    tokenizer = get_tokenizer(config.tokenizer_model_name)
-    preprocess_and_save_data(dataset_path=config.data_dir, tokenizer=tokenizer)
-    tokens, vocab_to_int, _ = load_preprocess()
+        raise ValueError(f"Unknown model: {model_name}")
 
-    # Create Dataset
-    train_loader = get_train_loader(
-        tokens=tokens,
-        config=config,
-        pad_token_id=tokenizer.pad_token_id,
-        model_name=model_name,
-    )
-
-    vocab_size = len(vocab_to_int)
-    output_size = vocab_size
-
-    model = get_model(
-        model_name=model_name,
-        vocab_size=vocab_size,
-        output_size=output_size,
-    )
-
-    device = torch.device(config.device_name)
-    model.to(device)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-    criterion = nn.CrossEntropyLoss()
-
+    # Train model
     model.train_model(
-        train_loader=train_loader,
-        batch_size=config.text_batch_size,
-        optimizer=optimizer,
-        criterion=criterion,
-        n_epochs=config.num_epochs,
+        train_data,
+        val_data,
+        block_size=8,
+        batch_size=8,
+        learning_rate=1e-2,
         device=device,
-        show_every_n_batches=config.show_every_n_batches,
+        eval_iters=200,
+        max_iters=3000,
+        eval_interval=300
     )
 
-    save_model(f"../trained_models/trained_{model}_{time.time()}.pt", model)
-    print("Model Trained and Saved")
+    # Save model
+    timestamp = int(time.time())
+    save_model(f"checkpoints/{model_name}_{dataset}_{timestamp}.pt", model=model)
 
-    # _, vocab_to_int, _ = load_preprocess()
-    # trained_rnn = load_model(f"../trained_models/trained_{model}.pt")
+    # Generate sample
+    context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
 
-    # if config.should_generate_text:
-    #     pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
-
-    #     for prime_text in config.start_text_generated:
-    #         encoded = tokenize_text(tokenizer=tokenizer, text=prime_text)
-    #         prime_id = encoded[-1] if encoded else pad_token_id
-
-    #         generated_script = generate(
-    #             rnn=trained_rnn,
-    #             prime_id=prime_id,
-    #             pad_token_id=pad_token_id,
-    #             device=device,
-    #             predict_len=config.generated_token_length,
-    #         )
-
-    #         print(detokenize_text(tokenizer=tokenizer, tokens=generated_script))
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Training pipeline")
     parser.add_argument('model', choices=['bigram', 'rnn', 'transformers'], help='The model architecture')
+    parser.add_argument('dataset', choices=['el_quijote'], help='Dataset')
+    args = parser.parse_args()
+
+    train_workflow(args.model, args.dataset)
